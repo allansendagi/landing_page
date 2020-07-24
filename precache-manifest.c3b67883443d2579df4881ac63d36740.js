628b22f620454b2881e7da389b1096b0ce340176
allansendagi <sendagiallan87@gmail.com>
2020-07-24 21:19:14 +0400

    Updates

---
+++ precache-manifest.c3b67883443d2579df4881ac63d36740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b01d3b8a18e88e71622feb622c84066",
+    "url": "/allansendagi/landing_page/index.html"
+  },
+  {
+    "revision": "6552345306af958c8887",
+    "url": "/allansendagi/landing_page/static/css/main.6577e2a8.chunk.css"
+  },
+  {
+    "revision": "f027b3b5ca80f1296dcd",
+    "url": "/allansendagi/landing_page/static/js/2.ad80b5d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/allansendagi/landing_page/static/js/2.ad80b5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6552345306af958c8887",
+    "url": "/allansendagi/landing_page/static/js/main.c4d77e27.chunk.js"
+  },
+  {
+    "revision": "3ac49401681cc09d44bc",
+    "url": "/allansendagi/landing_page/static/js/runtime-main.ad20f9bd.js"
+  }
+]);
